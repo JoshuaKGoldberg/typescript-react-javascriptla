8ca562407f39cd456bc6eb64d724783a7722ea1b
Sylvana Santos <sylvana@codecademy.com>
2021-07-27 18:44:13 -0400

    simplify starting point and replace type with interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
 import './App.css';
 import { Card } from './Card';
 
-const logCardGreeting = () => {
-  console.log("I am a fun card!")
-}
+// const logCardGreeting = () => {
+//   console.log("I am a fun card!")
+// }
 
 export const App = () => {
   return (
     <div className="App">
       <header className="App-container">
         <Card 
-          courseName="Front-End Engineer" 
           courseType="Career Path" 
-          clickHandler={logCardGreeting}
           showProLabel={true}
-          tag="Job Essentials"
         />
       </header>
     </div>
